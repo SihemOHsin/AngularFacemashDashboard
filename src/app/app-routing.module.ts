8f7181f05cacc9bc587aa8ff82f7851f8b94a7cf
sihem hsin <sihemouledhsin@gmail.com>
2023-10-08 21:26:42 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+const routes: Routes = [
+  {path:'',component:FrontLayoutComponent,children:[
+    {path:'',loadChildren:()=>import('./views/front/home/home.module').then(m=>m.HomeModule)},
+    {path:'loginuser',loadChildren:()=>import('./views/front/loginuser/loginuser.module').then(m=>m.LoginuserModule)},
+    {path:'register',loadChildren:()=>import('./views/front/register/register.module').then(m=>m.RegisterModule)},
+    {path:'vote',loadChildren:()=>import('./views/front/vote/vote.module').then(m=>m.VoteModule)},
+    {path:'score',loadChildren:()=>import('./views/front/score/score.module').then(m=>m.ScoreModule)}
+  ]},
+  {path:'admin',component:AdminLayoutComponent,children:[
+    {path:'',loadChildren:()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
+    {path:'dashboard',loadChildren:()=>import('./views/admin/dashboard/dashboard.module').then(m=>m.DashboardModule)},
+    {path:'loginadmin',loadChildren:()=>import('./views/admin/loginadmin/loginadmin.module').then(m=>m.LoginadminModule)},
+    {path:'allusers',loadChildren:()=>import('./views/admin/allusers/allusers.module').then(m=>m.AllusersModule)},
+    {path:'adduser',loadChildren:()=>import('./views/admin/adduser/adduser.module').then(m=>m.AdduserModule)}
+
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
